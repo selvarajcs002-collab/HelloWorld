45aba4efefab5ac066236e2c8ae2aecdca6dce07
selvaraj751998 <selvarajcs01@example.com>
2026-02-21 13:13:51 +0530

    refactor: merge `add` function test into `sayHello` function test.

--- test.js
+++ test.js
@@ -1,18 +1,10 @@
 const sayHello = require('./app');
 const add = require('./app');
 
-if (sayHello() === "Hello World") {
+if (sayHello() === "Hello World" && add(1, 2) == 3) {
     console.log("Test Passed ✅");
     process.exit(0);
 } else {
     console.log("Test Failed ❌");
-    process.exit(1);
-}
-
-if (add(1, 2) == 3) {
-    console.log("Add Test Passed ✅");
-    process.exit(0);
-} else {
-    console.log("Add Test Failed ❌");
     process.exit(1);
 }
